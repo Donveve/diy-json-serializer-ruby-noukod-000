5225be3bb22a58e9e10f01cd02941fa6d84d41b6
Veniel Dorestave <veniel@noukod.com>
2019-03-15 13:28:07 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,47 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+    @post = Post.create(post_params)
+    @post.save
+    redirect_to post_path(@post)
+  end
+
+  def edit
+  end
+
+  def update
+    @post.update(post_params)
+    redirect_to post_path(@post)
+  end
+
+  def post_data
+    post = Post.find(params[:id])
+    render plain: post.description
+  endclass PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
+  def post_data
+    post = Post.find(params[:id])
+    render json: PostSerializer.serialize(post)
+  end
+
   def index
     @posts = Post.all
   end
@@ -30,6 +71,18 @@ class PostsController < ApplicationController
     post = Post.find(params[:id])
     render plain: post.description
   end
+
+private
+  # Use callbacks to share common setup or constraints between actions.
+  def set_post
+    @post = Post.find(params[:id])
+  end
+
+  # Never trust parameters from the scary internet, only allow the white list through.
+  def post_params
+    params.require(:post).permit(:title, :description)
+  end
+end
 
 private
   # Use callbacks to share common setup or constraints between actions.
