57c042371ebc3c0b7d17e6bf40d0520f8bceb1ef
Veniel Dorestave <veniel@noukod.com>
2019-03-15 13:32:20 +0000

    Automatically backed up by Learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,52 +1,12 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update]
 
-  def post_data
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
-  end
-
   def index
     @posts = Post.all
   end
 
   def show
-  end
-
-  def new
-    @post = Post.new
-  end
-
-  def create
-    @post = Post.create(post_params)
-    @post.save
-    redirect_to post_path(@post)
-  end
-
-  def edit
-  end
-
-  def update
-    @post.update(post_params)
-    redirect_to post_path(@post)
-  end
-
-  def post_data
-    post = Post.find(params[:id])
-    render plain: post.description
-  endclass PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
-
-  def post_data
-    post = Post.find(params[:id])
-    render json: PostSerializer.serialize(post)
-  end
-
-  def index
-    @posts = Post.all
-  end
-
-  def show
+    @post=Post.find_by(id:params[:id])
   end
 
   def new
@@ -72,17 +32,11 @@ class PostsController < ApplicationController
     render plain: post.description
   end
 
-private
-  # Use callbacks to share common setup or constraints between actions.
-  def set_post
-    @post = Post.find(params[:id])
-  end
+  def post_data
+    post=Post.find(params[:id])
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def post_params
-    params.require(:post).permit(:title, :description)
+    render json: PostSerializer.serialize(post)
   end
-end
 
 private
   # Use callbacks to share common setup or constraints between actions.
